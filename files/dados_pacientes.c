98a63d759477c0016450305488c0b4ec139f2be1
Ian-S1lva <ian.cruz.074@ufrn.edu.br>
2025-07-04 19:53:03 +0000

    tabela hash

--- files/dados_pacientes.c
+++ files/dados_pacientes.c
@@ -8,6 +8,152 @@
 
 capacidade = 50 registros
 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TAMANHO 10
+#define TAMANHO_NOME 30
+
+/*
+ex. de tabela:
+
+tamamanho = 10
+funcao = chave%tamanho
+
+hash 0 -> 10, 100
+hash 1 -> 51
+hash 2 -> NULL
+...
+hash 9 -> 99, 89, 39, 999
+
+*/
+
+
+//cada No tem uma chave, e aponta para o proximo no do mesmo HASH
+typedef struct No{
+  int chave; //lê apenas o numero, ex.: PAC001 -> 001
+
+  char nome[TAMANHO_NOME];
+  int prioridade;
+  int atendido;
+
+  struct No *proximo;
+} No;
+
+//tabela hash é um array, cada indice associa um hash
+typedef struct tabela_hash{
+  No *tabela[TAMANHO];
+}tabela_hash;
+
+
+//a funcao hash associa cada chave ao indice correspondente ao resto da divisao chave%tamanho na tabela hash
+int funcao_hash(int chave)
+{
+  return chave % TAMANHO;
+}
+
+//cria um novo NO
+No *criar_no(int chave)
+{
+  No *novo_no = (No *)malloc(sizeof(No));
+  if(novo_no){
+    novo_no->chave = chave; 
+    novo_no->proximo = NULL;
+  }
+  return novo_no;
+}
+
+//todos os indices hash sao zerados
+void inicializar_tabela(tabela_hash *tabela)
+{
+    for(int i = 0; i < TAMANHO; i++){
+        tabela->tabela[i] = NULL;
+    }
+}
+
+//insere chave no seu hash correspondente, o novo elemento inserido se torna o 1º na lista da tabela hash
+// ex.: hash i = NOVO elemento -> elemento antigo -> ... -> NULL
+void inserir(tabela_hash *tabela, int chave, char *nome, int prioridade, int atendido)
+{
+  int indice = funcao_hash(chave);
+  No* novo_no = criar_no(chave);
+
+  if(tabela->tabela[indice] == NULL){
+    tabela->tabela[indice] = novo_no;
+  }else{
+    novo_no->proximo = tabela->tabela[indice];
+    tabela->tabela[indice] = novo_no;
+  }
+
+  //inicializa atributos do novo no
+    for(int i = 0; i < TAMANHO_NOME; i++){
+    novo_no->nome[i] = nome[i];
+    }
+    novo_no->prioridade = prioridade;
+    novo_no->atendido = atendido;
+
+
+}
+
+//busca no hash correspondente a chave, se há um elemento com a chave
+//retorna sim ou nao
+int buscar(tabela_hash *tabela, int chave)
+{
+  int indice = funcao_hash(chave);
+  No *atual = tabela->tabela[indice];
+
+  while(atual != NULL){
+    if(atual->chave == chave){
+      return 1;
+    }
+      atual = atual->proximo;
+  }
+  
+  return 0;
+}
+
+// remove hash correspondente a chave, se existir
+void remover(tabela_hash *tabela, int chave)
+{
+  int indice = funcao_hash(chave);
+  No *atual = tabela->tabela[indice];
+  No *anterior = NULL;
+
+  while(atual != NULL)
+  {
+    if(atual->chave == chave){
+      if (anterior == NULL) {
+        tabela->tabela[indice] = atual->proximo;
+      }else{
+        anterior->proximo = atual->proximo;
+      }
+      free(atual);
+      return;
+    }
+    anterior = atual;
+    atual = atual->proximo;
+  }
+  
+}
+
+//NAO USADO
+void imprimir_tabela(tabela_hash* tabela)
+{
+  for(int i = 0; i < TAMANHO; i++){
+    printf("Índice %d: ", i);
+    No* atual = tabela->tabela[i];
+    while(atual != NULL){
+      printf("%d -> ", atual->chave);
+      atual = atual->proximo;
+    }
+    printf("NULL\n");
+  }
+}
+
+/*
 3 Verificar término de pacientes não atendidos na tabela hash:
 
 Caso todos os pacientes já tenham sido transferidos da tabela hash para o deque, nenhuma nova entrada será possível.
