fb017f6f5dcf33fb838620b9bcdd7f8b10c5b552
Ian-S1lva <ian.cruz.074@ufrn.edu.br>
2025-07-05 16:14:08 +0000

    teste 1

--- files/main.c
+++ files/main.c
@@ -5,30 +5,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h> 
+#include <unistd.h>
 
-#include "../dados_pacientes.h"
+#include "dados_pacientes.h"
 
 #define TAMANHO_LINHA 100
 
-int lerCSV(tabela_hash *tabela) {
-    FILE* arquivo = fopen("input/pacientes.csv", "r");
+int lerCSV(tabela_hash *tabela){
+    FILE* arquivo = fopen("pacientes.csv", "r");
     if (!arquivo) {
         perror("Arquivo pacientes.csv nao encontrado na pasta \"input\".\n");
         return 1;
     }
-  
+
     char linha[TAMANHO_LINHA];
 
-  int chave; //lê apenas o numero, ex.: PAC001 -> 001
-  char nome[TAMANHO_NOME];
-  int prioridade;
-  int atendido;
-
+    int chave; //lê apenas o numero, ex.: PAC001 -> 001
+    char nome[TAMANHO_NOME];
+    int prioridade;
+    int atendido;
+//TESTE
     while(fscanf(arquivo, "%s", linha) != -1){//enquanto ler linhas
-    
+
         //analise a entrada por campos
-    for(int i = 0, j = 0, k = 0  ; i < TAMANHO_LINHA; i++){
+        for(int i = 0, j = 0, k = 0  ; i < TAMANHO_LINHA; i++){
         // i percorre a linha lida
         // j conta os campos separados por ';'
         // k marcaa posição do último ';'
@@ -40,51 +40,56 @@ int lerCSV(tabela_hash *tabela) {
       6->  Atendido: Valor lógico 0 ou 1
         */
 
-        if(linha[i] == ';'){
-            char campo[30];
-            strncpy(campo, &linha[k], k-i+1);    
-            
-            if(j == 0){
-                int n = strlen(campo) - 1;
-                chave = campo[n] - '0' + 10*(campo[n-1] - '0') + 100*(campo[n-2] - '0');
-            }
-            else if(j == 1){
-                strncpy(nome,campo,strlen(campo));
-            }
-            else if(j == 5){
-                prioridade = campo[0] - '0';
-            }
-            else if(j == 6){
-                atendido = campo[0] - '0';
-            }
+            if(linha[i] == ';'){
+                char campo[30];
+                strncpy(campo, &linha[k], k-i+1);
 
-            //mais um ';'
-            j++;
-            k = i;
+                if(j == 0){
+                    int n = strlen(campo) - 1;
+                    chave = campo[n] - '0' + 10*(campo[n-1] - '0') + 100*(campo[n-2] - '0');
+                }
+                else if(j == 1){
+                    strncpy(nome,campo,strlen(campo));
+                }
+                else if(j == 5){
+                    prioridade = campo[0] - '0';
+                }
+                else if(j == 6){
+                    atendido = campo[0] - '0';
+                }
+                
+                //mais um ';'
+                j++;
+                k = i;
+            }
         }
+        //depois de analisar o campo, insere na tabela
+        inserir(tabela, chave, nome, prioridade, atendido);
     }
-    //depois de analisar o campo, insere na tabela
-    inserir(tabela, chave, nome, prioridade, atendido);
-}
-    
+
     fclose(arquivo);
     return 0;
 }
 
 
 int main(){
+    tabela_hash tabela;
+    inicializar_tabela(&tabela);
+    lerCSV(&tabela);
+    imprimir_tabela(&tabela);
 
     int cont = 0;
         while(cont < 5){
             sleep(2);
             cont++;
-            printf("%d",cont);
+            printf("%d\n",cont);
         }
 
 
-    
-/* 
-iterar ciclo (a cada 2 segundos)
+
+
+/*
+OK iterar ciclo (a cada 2 segundos)
 
 1. Atualiza leito
     Atualiza alta
@@ -97,7 +102,7 @@ registrar no log
 */
 
 //encerra quando dados_pacientes, fila_espera, lista_leitos estiverem todos vazios
-return 0;
+    return 0;
 }
 
 /* LOGS
